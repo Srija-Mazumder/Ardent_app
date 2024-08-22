70e28a9f4f3e72b83f665550f07833e2b324b3a2
Srija-Mazumder <srijaraimazumder@gmail.com>
2024-08-22 21:50:25 +0530

    Initial commit with folder-name

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './index.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+  <BrowserRouter>
+  <Header/>
+  <Routes>
+    <Route path="/" exact={true} element={<Dashboard/>}/>
+    <Route path="/Dashboard" exact={true} element={<Dashboard/>}/>
+  </Routes>
+  </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
